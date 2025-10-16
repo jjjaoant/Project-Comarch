f39b103bd4ab93d289f918cf444766f16441904d
Panita Donmuang <panipani.d903@gmail.com>
2025-10-16 18:44:40 +0700

    fix

--- test_Multiplication.asm
+++ test_Multiplication.asm
@@ -1,22 +1,46 @@
-lw      0       1       mcand   load multiplicand into $1
-        lw      0       2       mplier  load multiplier into $2
-        lw      0       3       pos1    $3 = 1 (for bit mask)
-        lw      0       4       pos16   $4 = 16 (bit counter, check 16 times)
-        add     0       0       5       $5 = 0 (result accumulator)
-loop    nand    2       3       6       $6 = ~(mplier & mask)
-        nand    6       6       6       $6 = mplier & mask
-        beq     0       6       skip    if bit is 0, skip addition
-        add     5       1       5       result += mcand
-skip    add     1       1       1       mcand = mcand << 1 (multiply by 2)
-        add     3       3       3       mask = mask << 1
-        lw      0       6       neg1    $6 = -1
-        add     4       6       4       counter--
-        beq     0       4       done    if counter == 0, done
-        beq     0       0       loop    continue loop
-done    add     5       0       1       move result to $1
+lw      0       1       mcand   load multiplicand
+        lw      0       2       mplier  load multiplier
+        add     0       0       7       $7 = 0 (sign counter)
+        lw      0       6       signBit $6 = 0x80000000
+        nand    1       6       3       check mcand sign
+        nand    3       3       3
+        beq     0       3       chkMp   if 0, mcand >= 0
+        nand    1       1       1       negate mcand
+        lw      0       3       pos1
+        add     1       3       1
+        add     7       3       7       sign++
+chkMp   nand    2       6       3       check mplier sign
+        nand    3       3       3
+        beq     0       3       mult    if 0, mplier >= 0
+        nand    2       2       2       negate mplier
+        lw      0       3       pos1
+        add     2       3       2
+        add     7       3       7       sign++
+mult    lw      0       3       pos1    start multiply
+        lw      0       4       pos16
+        add     0       0       5
+loop    nand    2       3       6
+        nand    6       6       6
+        beq     0       6       skip
+        add     5       1       5
+skip    add     1       1       1
+        add     3       3       3
+        lw      0       6       neg1
+        add     4       6       4
+        beq     0       4       chkSg
+        beq     0       0       loop
+chkSg   lw      0       3       pos1
+        nand    7       3       6
+        nand    6       6       6
+        beq     0       6       done
+        nand    5       5       5
+        lw      0       3       pos1
+        add     5       3       5
+done    add     5       0       1
         halt
 pos1    .fill   1
 neg1    .fill   -1
 pos16   .fill   16
-mcand   .fill   32766
-mplier  .fill   10383
+signBit .fill   -2147483648
+mcand   .fill   -32766
+mplier  .fill   -10383
